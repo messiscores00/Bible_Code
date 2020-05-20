b6ad1248cc8017d06f89ee3f893b4059e60a8296
Ryan Sheehy <ryansheehy0@gmail>
2020-05-19 19:40:24 -0700

    added search for 1 word

--- word_search.java
+++ word_search.java
@@ -1,27 +1,60 @@
-
 public class word_search
 {
     String Input;
     int i = 0;
-    public word_search(String input)
+    int first = 0;
+    int[] Return = new int[2];
+    int[] spacing;
+    int[] Result;
+    public word_search(String input, String search)
     {
         Input = new String(input);
+        Result = this.search_for_word(search,0);
     }
-    public int[] search_for_word(String searchWord, int space){
-        int spacing = space;
-        int[] Return = {0,0};
+    private int[] search_for_word(String searchWord, int a){
+        if(a==0){
+            spacing = new int[searchWord.length()-1];
+            while(i<Input.length()){
+                if(Input.charAt(i) == searchWord.charAt(0)){
+                    first = i;
+                    i++;
+                    this.search_for_word(searchWord,1);
+                }
+                i++;
+            }
+        }
+        //--------------------------------
         while(i<Input.length()){
-            if(Input.charAt(i) == searchWord.charAt(0)){
-                try{
-                    this.search_for_word(searchWord.substring(1), spacing);
-                }catch(IndexOutOfBoundsException test){
-                    Return[0] = 1;
-                    Return[2] = spacing;
-                    return Return;
+            if(Input.charAt(i) == searchWord.charAt(a)){
+                i++;
+                if(a+1<searchWord.length()){
+                    this.search_for_word(searchWord,a+1);
+                }else{
+                    if(this.equals(spacing)){
+                        Return[0] = first;
+                        Return[1] = spacing[0];
+                        return Return;
+                    }else{
+                        
+                    }
                 }
             }
             i++;
+            spacing[a-1]++;
         }
         return Return;
+    }
+    
+    private boolean equals(int[] array){
+        for(int i = 1; i<array.length ; i++){
+            if(array[i-1] != array[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public int[] getResult(){
+        return Result;
     }
 }
